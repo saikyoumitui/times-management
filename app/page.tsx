8d4f26806b15f35c7c1746d7aadf4cf419a3fba6
Miyabi <miyabi1704@icloud.com>
2026-02-18 13:40:17 +0900

    ボタン横並び

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
     <div className="flex flex-col items-center gap-6 mt-20">
       <h1 className="text-3xl font-bold">時間管理アプリ</h1>
 
+
+<div className="flex gap-4">  
       <button className="px-6 py-3 bg-green-500 text-white rounded-lg shadow">
         出勤
       </button>
@@ -11,16 +13,16 @@ export default function Home() {
         退勤
       </button>
    
-     　<button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow">
-    　　休憩開始
-　　　　</button>
+     <button className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow">
+        休憩開始
+    </button>
 
       <button className="px-6 py-3 bg-yellow-500 text-white rounded-lg shadow">
         休憩終了
       </button>
-
+  </div>
       
     </div>
   )
-}
 
+}
